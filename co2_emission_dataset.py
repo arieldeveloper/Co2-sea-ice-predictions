71d2721503826a3bc28fed6e0aaa2f4f98563941
Umayrah902 <75751783+Umayrah902@users.noreply.github.com>
2020-12-09 21:16:12 -0500

    Update co2_emission_dataset.py

--- co2_emission_dataset.py
+++ co2_emission_dataset.py
@@ -5,10 +5,11 @@ import requests
 url = 'https://raw.githubusercontent.com/arieldeveloper/CSC110-Final/main/datasets/owid-co2-data.csv?token=ASB6CZ6QOER7EN3GIBO5CBS73KHZW'
 response = requests.get(url)
 
-with open('co2_emission_dataset.csv', 'w') as f:
-    writer = csv.writer(f)
-    for line in response.iter_lines():
-        writer.writerow(line.decode('utf-8').split(','))
+url_content = response.content
+csv_file = open('co2_emission_dataset.csv', 'wb')
+csv_file.write(url_content)
+csv_file.close()
 
 # According to https://www.ucsusa.org/resources/each-countrys-share-co2-emissions, these were the top five countries
 # with the leading emission of co2 in 2018: China, United States, India, Russian Federation, Japan
+
