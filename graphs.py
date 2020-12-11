221aaa3ebc4e132998971a90d7c517016a29bfd0
ariel <arielchoum@gmail.com>
2020-12-11 15:58:12 -0500

    add graphing using plotly

--- graphs.py
+++ graphs.py
@@ -6,6 +6,7 @@ import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import plotly.express
 import pandas
+from plotly.subplots import make_subplots
 
 import plotly.graph_objects as go
 from typing import List, Tuple
@@ -17,7 +18,7 @@ class Linegraph:
         self.x = x
         self.y = y
 
-def plot(data) -> None:
+def plot(data, title) -> None:
     """Plot a line graph given the data
     """
     x, y = make_x_y_lists(data)
@@ -29,3 +30,27 @@ def plot(data) -> None:
 
     fig.show()
 
+
+def multiple_graphs_plot(data1, data2, data3, title) -> None:
+    """Plot a line graph given the data
+    """
+    x1, y1 = make_x_y_lists(data1)
+    x2, y2 = make_x_y_lists(data2)
+    x3, y3 = make_x_y_lists(data3)
+
+    fig = make_subplots(rows=3, cols=1)
+
+    fig.add_trace(go.Scatter(x=x1, y=y1,
+                             mode='lines',
+                             name='lines'), row = 1, col=1)
+
+    fig.add_trace(go.Scatter(x=x2, y=y2,
+                             mode='lines',
+                             name='lines'), row=2, col=1)
+
+    fig.add_trace(go.Scatter(x=x3, y=y3,
+                             mode='lines',
+                             name='lines'), row=3, col=1)
+
+    fig.update_layout(height=900, width=1000, title_text=title)
+    fig.show()
--- main.py
+++ main.py
@@ -38,5 +38,9 @@ sea_ice_data = extract_sea_ice('datasets/sea-ice-data.csv')
 co2_data = extract_co2emission_top_five('datasets/co2-data.csv')
 temp_data = extract_temperatures('datasets/temperature-data.csv')
 
-# Graph the data
-graphs.plot(sea_ice_data)
+#
+
+# Sea ice over time graph
+# graphs.plot(sea_ice_data, title="Sea ice concentration over time")
+
+graphs.multiple_graphs_plot(co2_data, sea_ice_data, temp_data, title="Sea ice concentration over time")
