1e767206bd40091858c246e5f4cfa2b568a668e0
oliverlaranjeira <75751738+oliverlaranjeira@users.noreply.github.com>
2020-12-10 21:46:32 -0300

    Updated to create all datasets

--- co2_emission_dataset.py
+++ co2_emission_dataset.py
@@ -10,6 +10,21 @@ csv_file = open('co2_emission_dataset.csv', 'wb')
 csv_file.write(url_content)
 csv_file.close()
 
+url = 'https://raw.githubusercontent.com/arieldeveloper/CSC110-Final/main/datasets/GlobalTemperatures.csv?token=ASB6COVWUJSSHAUIRB27S7C73PZGY'
+response = requests.get(url)
+
+url_content = response.content
+csv_file = open('global_temp_dataset.csv', 'wb')
+csv_file.write(url_content)
+csv_file.close()
+
+url = 'https://raw.githubusercontent.com/arieldeveloper/CSC110-Final/main/datasets/sea_ice.csv?token=ASB6COS2NYLTHQRCIXSCWNS73PZKQ'
+response = requests.get(url)
+
+url_content = response.content
+csv_file = open('sea_ice_index_dataset.csv', 'wb')
+csv_file.write(url_content)
+csv_file.close()
+
 # According to https://www.ucsusa.org/resources/each-countrys-share-co2-emissions, these were the top five countries
 # with the leading emission of co2 in 2018: China, United States, India, Russian Federation, Japan
-
