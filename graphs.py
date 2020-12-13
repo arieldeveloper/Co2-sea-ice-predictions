b8d60b802c98a7d13b961dda70ce4c4199dd0b5c
Ariel Chouminov <arielchoum@gmail.com>
2020-12-12 20:19:17 -0500

    Update graphs.py

--- graphs.py
+++ graphs.py
@@ -12,29 +12,6 @@ from typing import List, Tuple
 import csv
 from dataset_extract import make_x_y_lists
 
-
-class Linegraph:
-    """
-    Defines an instance of the class Linegraph.
-    """
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-
-def plot(data, title) -> None:
-    """Plot a line graph given the data
-    """
-    x, y = make_x_y_lists(data)
-    fig = go.Figure()
-
-    fig.add_trace(go.Scatter(x=x, y=y,
-                             mode='lines',
-                             name='lines'))
-
-    fig.show()
-
-
 def three_graphs_plot(data1: Dict[int, float], data2: Dict[int, float], data3: Dict[int, float], title: str) -> None:
     """Plot three line graphs given the three datasets.
 
