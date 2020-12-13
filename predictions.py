0ab24f8e10fa68c936e6995802c04743ad9d75f1
Ariel Chouminov <arielchoum@gmail.com>
2020-12-12 19:37:56 -0500

    add linear regression function

--- predictions.py
+++ predictions.py
@@ -5,52 +5,52 @@ Description of module
 import numpy as np
 import matplotlib.pyplot as plt
 from dataset_extract import *
-
-# Importing the dataset
-co2_data = extract_co2emission_top_five('datasets/co2-data.csv')
-
-# Creating the x and y arrays as numpy arrays
-X = np.array([key for key in co2_data])
-y = np.array([co2_data[key] for key in co2_data])
-
-# Reshape the
-X = X.reshape(len(X), 1)
-
-# Splitting the dataset into the Training set and Test set
+from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, train_size=0.8, random_state=0)
+def create_linear_regression(x_values, y_values):
 
-# Feature Scaling
-"""from sklearn.preprocessing import StandardScaler
-sc_X = StandardScaler()
-X_train = sc_X.fit_transform(X_train)
-X_test = sc_X.transform(X_test)
-sc_y = StandardScaler()
-y_train = sc_y.fit_transform(y_train)"""
+    # Creating the x and y arrays as numpy arrays
 
-# Linear regression model being applied to the sets of data we have
-from sklearn.linear_model import LinearRegression
+    x = np.array(x_values)
+    y = np.array(y_values)
 
-regressor = LinearRegression()
+    # Reshape the
+    x = x.reshape(len(x), 1)
 
-regressor.fit(X_train, y_train)
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.20, train_size=0.8, random_state=0)
 
-# Predicting the test set results
-y_pred = regressor.predict(X_test)
+    regressor = LinearRegression()
 
-# Visualizing the training set results
-plt.scatter(X_train, y_train, color='red')
-plt.plot(X_train, regressor.predict(X_train), color='blue')
-plt.title('C02 growth (training set)')
-plt.xlabel('years')
-plt.ylabel('co2 values')
-plt.show()
+    regressor.fit(x_train, y_train)
 
-# Visualizing the Test set results
-plt.scatter(X_test, y_test, color='red')
-plt.plot(X_train, regressor.predict(X_train), color='blue')
-plt.title('Co2 Growth (Test set)')
-plt.xlabel('years')
-plt.ylabel('c02 values')
-plt.show()
+    # Predicting the test set results
+    y_pred = regressor.predict(x_test)
+
+    # Calculate slope and intersept
+    slope = regressor.coef_[0]
+    intercept_value = regressor.intercept_
+
+    # Visualizing the training set results
+    plt.scatter(x_train, y_train, color='red')
+    plt.plot(x_train, regressor.predict(x_train), color='blue')
+    plt.title('C02 growth (training set)')
+    plt.xlabel('years')
+    plt.ylabel('co2 values')
+    plt.show()
+
+    # Visualizing the Test set results
+    plt.scatter(x_test, y_test, color='red')
+    plt.plot(x_train, regressor.predict(x_train), color='blue')
+    plt.title('Co2 Growth (Test set)')
+    plt.xlabel('years')
+    plt.ylabel('c02 values')
+    plt.show()
+    return slope, intercept_value
+
+def line_output(m, b, x):
+    """Output a y value given an x and the m and b value of the line
+    """
+    y = (m * x) + b
+    return y
+
